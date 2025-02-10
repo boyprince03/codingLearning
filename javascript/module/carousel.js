43bf1cc2132ab4ca1915a70f847b29035380dbce
Steve <boyprincemagician@gmail.com>
2025-02-10 22:11:11 +0800

    complete 輪播圖function

--- javascript/module/carousel.js
+++ javascript/module/carousel.js
@@ -1,4 +1,4 @@
-//基礎輪播功能
+//基礎輪播功能 3秒倫波
 export function Banner(){
     // 宣告變數設定為圖片的索引值
     let currentIndex = 0;
@@ -8,6 +8,21 @@ export function Banner(){
     const images=document.querySelectorAll("#Carousel01 >li");
     // 常數:讀取圖片的張(總)數(index 長度)
     const totalImages = images.length;
+
+    // 2025新增
+    // 修正區域
+    // 手動點擊上、下張
+    /*
+    圖片會立即切換
+    自動撥放停止
+    如果三秒無動作，自動波放重啟
+    */
+//    快速點擊:需無異常行為產生: setTimeout()確保setInterval()只有一個
+    // 存放自動撥放的定時器
+    let autoPlayInterval;
+    // 存放延遲重啟的定時器
+    let restarTimeout;
+
     // 顯示下一張圖片的函式
     function showNextImage(){
         //移除目前顯示中的類別(圖片)
@@ -26,9 +41,43 @@ export function Banner(){
         // 顯示上一張圖片
         images[currentIndex].classList.add("active");
     }
+
+
+    // 啟動自動輪播function
+    function startAutoPlay(){
+        autoPlayInterval= setInterval(showNextImage, 3000);
+    }
+    // 停止自動輪播
+    function stopAutoPlay(){
+        // 清除定時器
+        clearInterval(autoPlayInterval);
+        // 防止定時器重複
+        clearTimeout(restarTimeout);
+    }
+    // 延遲三秒後自動重啟輪播
+    function restarAutoPlay(){
+        // setTimeout是以知函數
+        restarTimeout=setTimeout(startAutoPlay, 3000);
+        // 自動輪播計時器秒:1000=1秒
+    }
+    // 沒有停止、延遲
+    startAutoPlay()
+
     // 按鈕控制
-    document.getElementById("prevBtn").addEventListener('click', showPrevImage);
-    document.getElementById("nextBtn").addEventListener('click', showNextImage);
-    // 自動輪播計時器秒:1000=1秒
-    setInterval(showNextImage, 3000);
+    document.getElementById("prevBtn").addEventListener('click', ()=>{ // ()=>{}代表function，可同時執行{}中的函式
+        // 停止自動輪播
+        stopAutoPlay();
+        // 顯示上一張圖片
+        showPrevImage();
+        // 偵測有沒有延遲三秒，後即自動撥放
+        restarAutoPlay();
+    });
+    document.getElementById("nextBtn").addEventListener('click', ()=>{
+        stopAutoPlay();
+        showNextImage();
+        restarAutoPlay();
+    });
+    
+    
 }
+
